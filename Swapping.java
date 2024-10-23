7f8873d4c7ab365ffe3c0312fbc0348f688256ac
ajayvenkatyatekare <ajayyatekare16@gmail.com>
2024-10-23 13:06:25 +0530

    git

---
+++ Swapping.java
@@ -0,0 +1,15 @@
+
+public class Swapping
+{
+  public static void main(String[] args)
+  {
+	  int a=10, b=20;
+	System.out.println("before swapinmg: a = "+ a +", b=" + b);
+	
+	 a = a+b;
+	 b = a-b;
+	 a = a-b;
+	System.out.println("after sawaping: a = "+ a +", b = "+b); 
+	  
+  }
+}
